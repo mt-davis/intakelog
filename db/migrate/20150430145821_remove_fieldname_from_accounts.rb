894b270996d50269b6f5ac8543bd27b18ffa38ed
M.T. Davis <marquese.davis@gmail.com>
2015-05-01 13:49:46 +0000

    Fixing Heroku DB

--- db/migrate/20150430145821_remove_fieldname_from_accounts.rb
+++
@@ -1,7 +0,0 @@
-class RemoveFieldnameFromAccounts < ActiveRecord::Migration
-  def change
-    remove_column :accounts, :program, :text
-    remove_column :accounts, :program_name, :string
-    remove_column :accounts, :program_program, :text
-  end
-end
