978b24385e800c88843ae8112379f97c2501f3b3
M.T. Davis <marquese.davis@gmail.com>
2016-01-29 16:20:21 +0000

    added sendgrid to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,12 +86,12 @@ config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
 
 config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
+address: "smtp.sendgrid.net",
 port: 587,
-domain: ENV["GMAIL_DOMAIN"],
+domain: "heroku.com",
 authentication: "plain",
 enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
+user_name: ENV["SENDGRID_USERNAME"],
+password: ENV["SENDGRID_PASSWORD"]
 }
 end
